59bb17b99c4ade6b01f2591abf9eb5ffd8c3d152
Giovanna Menezes <129130075+giovannamnz@users.noreply.github.com>
2023-09-05 21:17:06 -0300

    Add files via upload

---
+++ 04/testes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Construir um algoritmo que leia um número e imprima se ele é 
+    // igual a 5, a 200, a 400, se está no intervalo entre 500 e 1000, 
+    //ou se ela está fora dos escopos anteriores.
+
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    
+
+    if (num = 5)
+    {
+        printf("%d", num);
+    }
+
+    if (num = 200)
+    {
+        printf("%d", num);
+    }
+
+    if(num == 400)
+    {
+        printf("%d", num);
+    }
+
+    if((num > 500) && (num < 1000))
+    {
+        printf("%d", num);
+    }
+
+    else
+    {
+        printf("Numero fora dos escopos");
+    }
+        
+
+    return 0;
+}
